09620418d0860dd2440e8c76b0a1b6c8e7486b31
nati-m <it.natnael.mekonnen@gmail.com>
2021-06-19 20:01:31 +0300

    added animation -triggered by mouse click

--- src/components/RollingBallScene.vue
+++ src/components/RollingBallScene.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Rolling Ball</h1>
-    <div class="scene" ref="scene"></div>
+    <div class="scene" ref="scene" @click="moveBall"></div>
   </div>
 </template>
 
@@ -17,13 +17,57 @@ export default {
       scene: null,
       camera: null,
       renderer: null,
+      mesh: {
+        sphere: null,
+      },
+
+      eventLocation: null,
+      speed: 20,
     };
   },
 
   methods: {
-    animate(mesh) {
-      mesh.position.set(-400, -150, 0);
+    animate() {
+      let animId = requestAnimationFrame(this.animate);
+      let [meshType, x, y] = this.eventLocation;
+
+      if (
+        this.mesh[meshType].position.x == x &&
+        this.mesh[meshType].position.y == y
+      ) {
+        console.log(this.mesh[meshType].position.x);
+        this.speed = 20;
+        cancelAnimationFrame(animId);
+      }
+      if (x > this.mesh[meshType].position.x)
+        this.mesh[meshType].position.x += this.speed;
+      else if (x < this.mesh[meshType].position.x)
+        this.mesh[meshType].position.x -= this.speed;
+      if (y > this.mesh[meshType].position.y)
+        this.mesh[meshType].position.y += this.speed;
+      else if (y < this.mesh[meshType].position.y)
+        this.mesh[meshType].position.y -= this.speed;
+
+      if (this.speed > 1.0) this.speed -= 0.5;
+
       this.renderer.render(this.scene, this.camera);
+    },
+
+    moveBall(e) {
+      console.log(
+        "X->" +
+          (e.offsetX - this.sceneCanvas.offsetWidth / 2) +
+          "/" +
+          this.sceneCanvas.offsetWidth,
+        "Y->" +
+          (e.offsetY - this.sceneCanvas.offsetHeight / 2) +
+          "/" +
+          this.sceneCanvas.offsetHeight
+      );
+      let x = parseInt(e.offsetX - this.sceneCanvas.offsetWidth / 2);
+      let y = parseInt(e.offsetY - this.sceneCanvas.offsetHeight / 2);
+      this.eventLocation = ["sphere", x, y];
+      this.animate(...this.eventLocation);
     },
   },
 
@@ -67,20 +111,20 @@ export default {
       color: 0xffffff,
       wireframe: true,
     });
-    var sphere = new THREE.Mesh(geometry, material);
-    sphere.position.x = -20;
+    this.mesh.sphere = new THREE.Mesh(geometry, material);
 
-    this.scene.add(sphere);
+    this.scene.add(this.mesh.sphere);
 
     ////////// RENDER SCENE
-    this.animate(sphere);
+    this.mesh.sphere.position.set(-400, -150, 0);
+    this.renderer.render(this.scene, this.camera);
   },
 };
 </script>
 
 <style scoped>
 * {
-  margin: 0%;
+  margin: 0;
   padding: 0;
 }
 
