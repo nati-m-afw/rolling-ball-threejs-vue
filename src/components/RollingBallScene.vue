2ffe995c383a682a6ecbed6a25af50ca78e48b9e
nati-m <it.natnael.mekonnen@gmail.com>
2021-06-19 16:11:33 +0300

    setup three.js in vue

---
+++ src/components/RollingBallScene.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h1>Rolling Ball</h1>
+    <div class="scene" ref="scene"></div>
+  </div>
+</template>
+
+<script>
+import * as THREE from "three";
+
+export default {
+  name: "RollingBallScene",
+
+  data() {
+    return {
+      sceneCanvas: null,
+      scene: null,
+      camera: null,
+      renderer: null,
+    };
+  },
+
+  methods: {},
+
+  mounted() {
+    ////// INITIALIZATION
+    this.sceneCanvas = this.$refs.scene;
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      this.sceneCanvas.clientWidth / this.sceneCanvas.clientHeight,
+      0.1,
+      1000
+    );
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.sceneCanvas.appendChild(this.renderer.domElement);
+
+    /////// CONFIGURING INIT SETTINGS
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0%;
+  padding: 0;
+}
+
+h1 {
+  text-align: center;
+}
+
+.scene {
+  display: inline;
+  align-content: center;
+  width: 100%;
+  height: 600px;
+}
+
+.scene canvas {
+  display: inline !important;
+  margin: auto;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <rolling-ball />
+  </div>
+</template>
+
+<script>
+import RollingBallScene from "../components/RollingBallScene.vue";
+
+export default {
+  name: "Index",
+  components: {
+    "rolling-ball": RollingBallScene,
+  },
+};
+</script>
+
+<style scoped></style>
